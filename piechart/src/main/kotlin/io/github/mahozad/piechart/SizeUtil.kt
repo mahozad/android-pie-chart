4cc01fa56f7c20b36305f24b6c10ee4f30fbd2ff
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-05-17 10:47:00 +0430

    Re-indent the code

--- piechart/src/main/kotlin/io/github/mahozad/piechart/SizeUtil.kt
+++ piechart/src/main/kotlin/io/github/mahozad/piechart/SizeUtil.kt
@@ -4,39 +4,39 @@ import android.util.Log
 import android.view.View
 import kotlin.math.min
 
-    /**
-     * Extracted the calculation logics to a separate class and function to be testable.
-     */
-    internal fun calculateWidthAndHeight(
-        widthMeasureSpec: Int,
-        heightMeasureSpec: Int
-    ): Pair<Int, Int> {
-        val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
-        val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
-        val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
-        val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
-        val min = min(widthSize, heightSize)
-        Log.d("SizeUtil", "width: ${View.MeasureSpec.toString(widthMeasureSpec)}")
-        Log.d("SizeUtil", "height: ${View.MeasureSpec.toString(heightMeasureSpec)}")
-        Log.d("SizeUtil", "min: $min")
-        return Pair(min, min)
-    }
+/**
+ * Extracted the calculation logics to a separate class and function to be testable.
+ */
+internal fun calculateWidthAndHeight(
+    widthMeasureSpec: Int,
+    heightMeasureSpec: Int
+): Pair<Int, Int> {
+    val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
+    val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
+    val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
+    val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
+    val min = min(widthSize, heightSize)
+    Log.d("SizeUtil", "width: ${View.MeasureSpec.toString(widthMeasureSpec)}")
+    Log.d("SizeUtil", "height: ${View.MeasureSpec.toString(heightMeasureSpec)}")
+    Log.d("SizeUtil", "min: $min")
+    return Pair(min, min)
+}
 
-    internal data class Coordinate(val x: Float, val y: Float)
+internal data class Coordinate(val x: Float, val y: Float)
 
-    /**
-     * Note that this function works with paddingLeft and PaddingRight
-     *  not paddingStart and paddingEnd.
-     */
-    internal fun calculateCenterCoordinate(
-        width: Int,
-        height: Int,
-        paddingLeft: Int,
-        paddingRight: Int,
-        paddingTop: Int,
-        paddingBottom: Int
-    ): Coordinate {
-        val centerX = (width + paddingLeft - paddingRight) / 2f
-        val centerY = (height + paddingTop - paddingBottom) / 2f
-        return Coordinate(centerX, centerY)
-    }
+/**
+ * Note that this function works with paddingLeft and PaddingRight
+ *  not paddingStart and paddingEnd.
+ */
+internal fun calculateCenterCoordinate(
+    width: Int,
+    height: Int,
+    paddingLeft: Int,
+    paddingRight: Int,
+    paddingTop: Int,
+    paddingBottom: Int
+): Coordinate {
+    val centerX = (width + paddingLeft - paddingRight) / 2f
+    val centerY = (height + paddingTop - paddingBottom) / 2f
+    return Coordinate(centerX, centerY)
+}
