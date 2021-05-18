849f40c39945e2022d47d2aaa2d52334318bd496
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-05-18 16:45:12 +0430

    Add a new module for testing the library

---
+++ test/src/main/kotlin/chart/test/TesterActivity.kt
@@ -0,0 +1,11 @@
+package chart.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TesterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
