924aa8776d8431bd70584f3b96d86a8c2ef7d115
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-08-06 17:59:03 +0430

    Rename and update ShowcaseActivity

--- test/src/main/kotlin/chart/test/ShowcaseActivity.kt
+++ test/src/main/kotlin/chart/test/ShowcaseActivity.kt
@@ -2,10 +2,15 @@ package chart.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ir.mahozad.android.PieChart
 
-class ShowCaseActivity : AppCompatActivity() {
+class ShowcaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val chart = findViewById<PieChart>(R.id.pieChart)
+        chart.setOnClickListener {
+            chart.overlayRatio = 0.8f
+        }
     }
 }
