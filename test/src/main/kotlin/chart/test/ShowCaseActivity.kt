fb44a52107dfde2db87ca9e09869d8bd5daaf708
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-08-06 16:33:01 +0430

    Rename TesterActivity to ShowCaseActivity

--- test/src/main/kotlin/chart/test/ShowCaseActivity.kt
+++ test/src/main/kotlin/chart/test/ShowCaseActivity.kt
@@ -3,7 +3,7 @@ package chart.test
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class TesterActivity : AppCompatActivity() {
+class ShowCaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
