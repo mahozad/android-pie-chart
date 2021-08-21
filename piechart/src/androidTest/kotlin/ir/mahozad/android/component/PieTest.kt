7e80c10534e52498409cfb12b1cbb48212fe65e2
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-08-21 11:42:18 +0430

    Remove unit tests measuring the time it takes a property to update

--- piechart/src/androidTest/kotlin/ir/mahozad/android/component/PieTest.kt
+++ piechart/src/androidTest/kotlin/ir/mahozad/android/component/PieTest.kt
@@ -1,6 +1,5 @@
 package ir.mahozad.android.component
 
-import android.graphics.Color
 import android.graphics.Typeface
 import androidx.test.platform.app.InstrumentationRegistry
 import ir.mahozad.android.Coordinates
@@ -8,12 +7,10 @@ import ir.mahozad.android.PieChart
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.util.FloatComparator
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
-import kotlin.system.measureTimeMillis
 
 /**
  * Could not run these test as unit tests because they and the class under test
@@ -46,187 +43,4 @@ class PieTest {
         arguments(0f, 0f, 400f, 600f, Coordinates(200f, 300f)),
         arguments(-100f, -200f, 500f, 700f, Coordinates(50f, 250f)),
     )
-
-    @Test fun changePieAngleForInsideLabel() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0f, 0f, PieChart.GradientType.SWEEP, 0f, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setStartAngle(30)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changePieAngleForOutsideLabel() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.OUTSIDE
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0f, 0f, PieChart.GradientType.SWEEP, 0f, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setStartAngle(30)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changePieAngleForOutsideCircularLabel() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.OUTSIDE_CIRCULAR_OUTWARD
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0f, 0f, PieChart.GradientType.SWEEP, 0f, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setStartAngle(30)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changePieHoleRatio() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val holeRatio = 0.33f
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0f, 0f, PieChart.GradientType.SWEEP, holeRatio, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setHoleRatio(0.19f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changeOverlayRatio() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val overlayRatio = 0.49f
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, overlayRatio, 0f, PieChart.GradientType.SWEEP, 0.3f, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setOverlayRatio(0.567f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changeOverlayAlpha() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val overlayAlpha = 0.89f
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0.34f, overlayAlpha, PieChart.GradientType.SWEEP, 0.3f, null, 0f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setOverlayAlpha(0.567f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changeGap() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 0f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0.34f, 0.34f, PieChart.GradientType.SWEEP, 0.3f, null, 3f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setGap(15f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changeLabelsSize() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 20f, 0, Typeface.DEFAULT, 0f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0.34f, 0.34f, PieChart.GradientType.SWEEP, 0.3f, null, 3f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setLabelsSize(15f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
-
-    @Test fun changeLabelIconsHeight() {
-        val slices = listOf(
-            PieChart.Slice(0.3f, Color.BLACK),
-            PieChart.Slice(0.1f, Color.BLACK),
-            PieChart.Slice(0.28f, Color.BLACK),
-            PieChart.Slice(0.32f, Color.BLACK)
-        )
-        val labelType = PieChart.LabelType.INSIDE
-        val width = 1000f
-        val height = 1000f
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val pie = Pie(context, width, height, null, null, 0, slices, 0f, labelType, 20f, 0, Typeface.DEFAULT, 17f, 0f, PieChart.IconPlacement.START, null, 0f, false, PieChart.DrawDirection.CLOCKWISE, 0.34f, 0.34f, PieChart.GradientType.SWEEP, 0.3f, null, 3f, PieChart.GapPosition.MIDDLE)
-
-        val duration = measureTimeMillis {
-            pie.setLabelIconsHeight(23f)
-        }
-
-        assertThat(duration).isLessThan(15)
-    }
 }
