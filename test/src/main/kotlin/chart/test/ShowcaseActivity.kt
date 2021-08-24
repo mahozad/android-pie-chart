cbd2c212709c4f77144c9e69e942cb338d5be355
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-08-24 21:44:31 +0430

    Make showcase app show random charts on click and simplify a composable

--- test/src/main/kotlin/chart/test/ShowcaseActivity.kt
+++ test/src/main/kotlin/chart/test/ShowcaseActivity.kt
@@ -9,9 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import ir.mahozad.android.PieChart
@@ -29,16 +30,32 @@ class ShowcaseActivity : AppCompatActivity() {
         )
 
         setContent {
-            val slices = rememberSaveable { mutableStateOf(sliceItems) }
+            var slices by rememberSaveable { mutableStateOf(sliceItems) }
             // MaterialTheme {
             PieChartView(slices) {
-                slices.value = listOf(
-                    PieChart.Slice(0.63f, Color.BLACK),
-                    PieChart.Slice(0.47f, Color.RED)
-                )
+                slices = generateRandomNumbers().map { PieChart.Slice(it, generateRandomColor()) }
             }
             // }
         }
+    }
+
+    /**
+     * Generate a list of random numbers that sum up to the base.
+     * Both the number of values and the values themselves are random.
+     *
+     * See [this post](https://stackoverflow.com/q/2640053).
+     */
+    private fun generateRandomNumbers(base: Int = 1): List<Float> {
+        val sizeRange = (1..8)
+        val numberRange = (1..50)
+        val selectedSize = sizeRange.random()
+        val randomNumbers = (1..selectedSize).map { numberRange.random() }
+        return randomNumbers.map { it.toFloat() / randomNumbers.sum() * base}
+    }
+
+    private fun generateRandomColor(): Int {
+        fun component() = (0..255).random()
+        return Color.rgb(component(), component(), component())
     }
 
     private fun animateStartAngle(chart: PieChart) {
@@ -53,25 +70,25 @@ class ShowcaseActivity : AppCompatActivity() {
     }
 
     @Composable
-    fun PieChartView(slicesState: MutableState<List<PieChart.Slice>>, onClick: () -> Unit) {
+    fun PieChartView(slicesState: List<PieChart.Slice>, onClick: () -> Unit) {
         AndroidView(
             modifier = Modifier
                 .fillMaxSize()
                 .clickable { onClick() },
             factory = { context ->
                 PieChart(context).apply {
-                    slices = slicesState.value
+                    slices = slicesState
                     labelType = PieChart.LabelType.INSIDE
                     legendsTitle = "Legends"
                     isLegendEnabled = true
                     isLegendsPercentageEnabled = true
-                    legendPosition = PieChart.LegendPosition.START
+                    legendPosition = PieChart.LegendPosition.BOTTOM
                 }
             },
             update = { chart ->
                 // View's been inflated or state read in this block has been updated
                 // Add logic here if necessary
-                chart.slices = slicesState.value
+                chart.slices = slicesState
             }
         )
     }
