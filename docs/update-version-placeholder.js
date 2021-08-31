df21dcaa964c7790e512c49f37ed9848876415f4
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-08-31 22:06:06 +0430

    Add script to replace the version placeholder with the latest version of the library

---
+++ docs/update-version-placeholder.js
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/q/43262121/
+// https://stackoverflow.com/q/36975619
+
+const URL = "https://api.github.com/repos/mahozad/android-pie-chart/releases/latest"
+fetch(URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    const version = jsonResponse.tag_name.substring(1);
+    document.body.innerHTML = document.body.innerHTML.replace("&lt;version&gt;", version);
+  });
