3fca209d42dc5691b63200498179049269a78296
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-09-19 23:15:22 +0430

    Add title to showcase activities

--- test/src/main/kotlin/chart/test/ShowcaseComposeActivity.kt
+++ test/src/main/kotlin/chart/test/ShowcaseComposeActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import ir.mahozad.android.DEFAULT_HOLE_RATIO
 import ir.mahozad.android.compose.PieChartCompose
 import ir.mahozad.android.compose.SliceCompose
@@ -26,20 +33,25 @@ class ShowcaseComposeActivity : ComponentActivity() {
         setContent {
             var slices by remember { mutableStateOf(defaultSlices) }
             var holeRatio by remember { mutableStateOf(DEFAULT_HOLE_RATIO) }
-            MaterialTheme {
-                PieChartCompose(
-                    Modifier
-                        .fillMaxSize()
-                        .clickable {
-                            slices = generateRandomNumbers().map { fraction ->
-                                SliceCompose(fraction, generateRandomColorCompose())
-                            }
-                            holeRatio = 0.2f /* OR random.nextFloat() */
-                        },
-                    slices,
-                    holeRatio,
+            Box(Modifier.fillMaxWidth(), Alignment.Center) {
+                Text(
+                    text = stringResource(id = R.string.title_compose),
+                    color = colorResource(id = R.color.title_compose_color),
+                    fontSize = dimensionResource(id = R.dimen.title_compose_size).value.sp
                 )
             }
+            PieChartCompose(
+                Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        slices = generateRandomNumbers().map { fraction ->
+                            SliceCompose(fraction, generateRandomColorCompose())
+                        }
+                        holeRatio = 0.2f /* OR random.nextFloat() */
+                    },
+                slices,
+                holeRatio,
+            )
         }
     }
 }
