3de26dee334ed40ad095519567e754ab59c3b097
Mahdi Hosseinzadeh <29678011+mahozad@users.noreply.github.com>
2021-12-07 01:02:10 -0800

    Refactor code
    Use arrow function instead of anonymous function

--- docs/update-version-placeholder.js
+++ docs/update-version-placeholder.js
@@ -2,11 +2,10 @@
 // https://stackoverflow.com/q/36975619
 
 const URL = "https://api.github.com/repos/mahozad/android-pie-chart/releases/latest"
+
 fetch(URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonResponse) {
+  .then(response => response.json())
+  .then(jsonResponse => {
     const version = jsonResponse.tag_name.substring(1);
     document.body.innerHTML = document.body.innerHTML.replace("&lt;version&gt;", version);
   });
