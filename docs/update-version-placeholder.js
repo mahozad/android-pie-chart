852cb3f6528f137f2de54f954e1c9818dd7fcce2
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-02-22 19:04:40 +0330

    Update the script to replace all version placeholders with a real value

--- docs/update-version-placeholder.js
+++ docs/update-version-placeholder.js
@@ -2,10 +2,11 @@
 // and https://stackoverflow.com/q/36975619
 
 const URL = "https://api.github.com/repos/mahozad/android-pie-chart/releases/latest"
+const PLACEHOLDER = /<version>|&lt;version&gt;/g
 
 fetch(URL)
   .then(response => response.json())
   .then(jsonResponse => {
-    const version = jsonResponse.tag_name.substring(1);
-    document.body.innerHTML = document.body.innerHTML.replace("&lt;version&gt;", version);
+    const version = jsonResponse["tag_name"].substring(1);
+    document.body.innerHTML = document.body.innerHTML.replace(PLACEHOLDER, version);
   });
