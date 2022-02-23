57caee89badfdcb03f3f81998a0f4016a7bd7926
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-02-23 20:57:23 +0330

    Refactor code

--- docs/highlight-code-snippets.js
+++ docs/highlight-code-snippets.js
@@ -3,10 +3,11 @@ updateCodeSnippetsThemeIfNecessary();
 
 /* Check the theme and if it is changed, enable the proper style */
 function updateCodeSnippetsThemeIfNecessary() {
-  let currentTheme = detectTheme();
   let darkStyleLink = document.querySelector(`link[title="dark-code"]`);
   let lightStyleLink = document.querySelector(`link[title="light-code"]`);
-  if (currentTheme.name === "dark") {
+  let theme = getUserThemeSelection();
+  if (theme === THEME_AUTO) theme = getSystemTheme();
+  if (theme === THEME_DARK) {
     darkStyleLink.removeAttribute("disabled");
     lightStyleLink.setAttribute("disabled", "disabled");
   } else {
--- docs/update-theme.js
+++ docs/update-theme.js
@@ -1,79 +1,90 @@
-updateTheme();
+const THEME_KEY = "theme";
+const THEME_ATTR = "data-theme";
+const THEME_DARK = "dark";
+const THEME_AUTO = "auto";
+const THEME_LIGHT = "light";
 
-function detectTheme() {
-    const storedTheme = localStorage.getItem("theme");
-    // Default to light theme
-    const theme = { mode: "custom", name: "light" }
-    if (storedTheme === "auto") {
-        theme.name = getSystemTheme();
-        theme.mode = "auto";
-    } else if (storedTheme === "dark") {
-        theme.name = "dark";
-        theme.mode = "custom";
-    }
-    return theme;
+updateTheme();
+// Wait for the document to be ready: https://stackoverflow.com/a/800010
+document.addEventListener("DOMContentLoaded", onDocumentReady);
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", updateTheme);
+
+function updateTheme() {
+    let theme = getUserThemeSelection();
+    if (theme === THEME_AUTO) theme = getSystemTheme();
+    document.documentElement.setAttribute(THEME_ATTR, theme);
+}
+
+function getUserThemeSelection() {
+    const defaultTheme = THEME_LIGHT;
+    const userSelection = localStorage.getItem(THEME_KEY);
+    return userSelection === null ? defaultTheme : userSelection;
 }
 
 function getSystemTheme() {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        return "dark";
+        return THEME_DARK;
     } else {
-        return "light";
-    }
-}
-
-function updateTheme() {
-    let theme = detectTheme();
-    if (theme.name === "dark") {
-        document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-        document.documentElement.setAttribute("data-theme", "light");
+        return THEME_LIGHT;
     }
 }
 
 // See https://stackoverflow.com/q/48316611
 function toggleTheme() {
-    let currentTheme = detectTheme();
-    if (currentTheme.mode === "auto") {
-        currentTheme.mode = "custom";
-        currentTheme.name = "light";
-        document.getElementById("letter-anim-hide").beginElement()
-        document.getElementById("core-anim-shrink").beginElement()
-        document.getElementById("rays-anim-show").beginElement()
-        document.getElementById("rays-anim-rotate").beginElement()
-    } else if (currentTheme.name === "dark") {
-        currentTheme.mode = "auto";
-        currentTheme.name = "-";
-        document.getElementById("eclipse-anim-go").beginElement()
-        document.getElementById("letter-anim-show").beginElement()
-    } else {
-        currentTheme.mode = "custom";
-        currentTheme.name = "dark";
-        document.getElementById("core-anim-enlarge").beginElement()
-        document.getElementById("rays-anim-hide").beginElement()
-        document.getElementById("eclipse-anim-come").beginElement()
+    let oldTheme = getUserThemeSelection();
+    let newTheme;
+    if (oldTheme === THEME_AUTO) {
+        newTheme = THEME_LIGHT;
+        animateButtonIconToLight();
+    } else if (oldTheme === THEME_DARK) {
+        newTheme = THEME_AUTO;
+        animateButtonIconToAuto();
+    } else /* if (theme === THEME_LIGHT) */ {
+        newTheme = THEME_DARK;
+        animateButtonIconToDark();
     }
-    localStorage.setItem("theme", currentTheme.mode === "auto" ? "auto" : currentTheme.name);
+    localStorage.setItem(THEME_KEY, newTheme);
     updateTheme();
 }
 
-window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", updateTheme);
+function animateButtonIconToLight() {
+    document.getElementById("letter-anim-hide").beginElement();
+    document.getElementById("core-anim-shrink").beginElement();
+    document.getElementById("rays-anim-show").beginElement();
+    document.getElementById("rays-anim-rotate").beginElement();
+}
 
-// Wait for the document to be ready: https://stackoverflow.com/a/800010
-document.addEventListener("DOMContentLoaded", () => {
-    let currentTheme = detectTheme();
-    if (currentTheme.mode === "auto") {
-        document.getElementById("circle").setAttribute("r", "10")
+function animateButtonIconToAuto() {
+    document.getElementById("eclipse-anim-go").beginElement();
+    document.getElementById("letter-anim-show").beginElement();
+}
+
+function animateButtonIconToDark() {
+    document.getElementById("core-anim-enlarge").beginElement();
+    document.getElementById("rays-anim-hide").beginElement();
+    document.getElementById("eclipse-anim-come").beginElement();
+}
+
+function onDocumentReady() {
+    setThemeButtonInitialIcon();
+    const toggleButton = document.getElementById("theme-switch");
+    toggleButton.style.visibility = "visible";
+    toggleButton.addEventListener("click", toggleTheme);
+}
+
+function setThemeButtonInitialIcon() {
+    let theme = getUserThemeSelection();
+    if (theme === THEME_AUTO) {
+        document.getElementById("circle").setAttribute("r", "10");
         document.getElementById("rays").setAttribute("opacity", "0");
         document.getElementById("letter").setAttribute("opacity", "1");
-    } else if (currentTheme.name === "dark") {
+    } else if (theme === THEME_DARK) {
         document.getElementById("circle").setAttribute("r", "10");
         document.getElementById("rays").setAttribute("opacity", "0");
         document.getElementById("eclipse").setAttribute("cx", "20");
+    } else /* if (theme === THEME_LIGHT) */ {
+        // Do nothing and show the icon as is
     }
-    const toggleButton = document.getElementById("theme-switch")
-    toggleButton.style.visibility = "visible";
-    toggleButton.addEventListener("click", toggleTheme);
-});
+}
