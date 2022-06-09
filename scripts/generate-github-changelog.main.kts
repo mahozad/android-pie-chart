7f6564e33e10d1d3b7e65c27590f80b8587fe80e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-06-09 20:56:38 +0430

    Update some comments and add an import statement

--- scripts/generate-github-changelog.main.kts
+++ scripts/generate-github-changelog.main.kts
@@ -6,6 +6,7 @@
 // @file:DependsOn("com.example:library:1.2.3")
 
 import java.io.File
+import java.util.regex.Pattern
 
 val inputFile = File("CHANGELOG.md")
 val outputFile = File("changelog.txt")
@@ -14,6 +15,7 @@ val versionHeaderLineRegex = Regex(""".*v\d+\.\d+\.\d+.* \(\d{4}-\d{2}-\d{2}\)""
  * The `?<hash>` is called a named capture group and the `\k<hash>` is called a back-reference.
  * See [this Stack Overflow post](https://stackoverflow.com/a/72177157/8583692)
  * and https://github.com/tc39/proposal-regexp-named-groups
+ * and https://youtu.be/54WEfLKtCGk?t=1086
  * and the javadoc of the Java class [Pattern] for more information.
  */
 val commitReferenceRegex = Regex("""\[`(?<hash>[\da-f]{8})`]\([^(]+\k<hash>/?\)""")
@@ -63,11 +65,15 @@ fun createReleaseBody() = inputFile
  * when hovering over them with mouse, a popup shows the commit/issue information.
  */
 fun String.cleanReferences() = this
-    // FIXME: Replace with the following in Kotlin 1.7 and higher:
-    //  match.groups["hash"]?.value ?: error("Regex group not found")
+    // FIXME: Replace with the following in Kotlin 1.8 and higher:
+    //  match -> match.groups["hash"]?.value ?: error("Regex group not found")
+    // See https://youtrack.jetbrains.com/issue/KT-51908/Provide-common-function-for-getting-regex-capture-group-by-name
+    //  and https://youtu.be/54WEfLKtCGk?t=1086
     .replace(commitReferenceRegex) { match -> match.groupValues[1] }
-    // FIXME: Replace with the following in Kotlin 1.7 and higher:
-    //  "#${match.groups["number"]?.value ?: error("Regex group not found")}"
+    // FIXME: Replace with the following in Kotlin 1.8 and higher:
+    //  match -> "#${match.groups["number"]?.value ?: error("Regex group not found")}"
+    // See https://youtrack.jetbrains.com/issue/KT-51908/Provide-common-function-for-getting-regex-capture-group-by-name
+    //  and https://youtu.be/54WEfLKtCGk?t=1086
     .replace(issueReferenceRegex) { match -> "#${match.groupValues[1]}" }
 
 fun String.substringBetween(start: String, end: String) = substringAfter(start).substringBefore(end)
