506bc32afcca9f900956dae50d9dea580fa4bdbe
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-06-09 21:32:46 +0430

    Add a comment

--- scripts/update-badges-if-needed.main.kts
+++ scripts/update-badges-if-needed.main.kts
@@ -8,6 +8,7 @@ val readmeFile = File("README.md")
 val readmeLines = readmeFile.readLines()
 val kotlinLine = readmeLines.single { it.contains("[Kotlin]:") }
 // See https://www.regular-expressions.info/lookaround.html
+// and https://youtu.be/54WEfLKtCGk?t=1086
 // and https://stackoverflow.com/a/2078953/8583692
 val kotlinVersionRegex = Regex("""(?<=kotlin-)\d[.]\d+[.]\d+(-\w*)?(?=-)""")
 val kotlinVersion = kotlinVersionRegex.find(kotlinLine)?.value
